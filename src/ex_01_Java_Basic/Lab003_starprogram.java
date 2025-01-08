bb557f56bb9ef9352deddc66f7cee504fa5a6295
Akshay <akshysodegaonkar@gmail.com>
2025-01-08 15:33:02 +0530

    5th jan

---
+++ src/ex_01_Java_Basic/Lab003_starprogram.java
@@ -0,0 +1,19 @@
+package ex_01_Java_Basic;
+
+import java.sql.SQLOutput;
+
+public class Lab003_starprogram {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+
+
+    }
+}
