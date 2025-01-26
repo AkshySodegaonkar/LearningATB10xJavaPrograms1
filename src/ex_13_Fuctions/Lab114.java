909e19c3a8526c080bbf3d955823b2499a3e1aea
Akshay <akshysodegaonkar@gmail.com>
2025-01-26 10:33:10 +0530

    26-JAN

---
+++ src/ex_13_Fuctions/Lab114.java
@@ -0,0 +1,11 @@
+package ex_13_Fuctions;
+import java.lang.Math;
+public class Lab114 {
+    public static void main(String[] args) {
+
+        int result = Math.max(3,4);
+        System.out.println(result);
+
+
+    }
+}
